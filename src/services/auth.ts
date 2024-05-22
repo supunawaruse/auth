adbb633508be818e5191ccf511e2ee2787a78eff
supunawaruse <supunawa@gmail.com>
2024-05-22 08:57:49 +0530

    fdf

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,9 +4,9 @@ import AzureADProvider from 'next-auth/providers/azure-ad';
 const authOptions: NextAuthConfig = {
     providers: [
         AzureADProvider({
-            clientId: process.env.NEXT_PUBLIC_NEXTAUTH_AZURE_AD_CLIENT_ID,
-            clientSecret: process.env.NEXT_PUBLIC_NEXTAUTH_AZURE_AD_CLIENT_SECRET,
-            tenantId: process.env.NEXT_PUBLIC_NEXTAUTH_AZURE_AD_TENANT_ID,
+            clientId: process.env.AUTH_AZURE_AD_ID,
+            clientSecret: process.env.AUTH_AZURE_AD_SECRET,
+            tenantId: process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID,
             authorization: {
                 params: { scope: 'openid email profile User.Read  offline_access' }
             }
